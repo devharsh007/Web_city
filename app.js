b56183b6361e989a929290cde1a17b8fb19f3820
devharsh007 <hs707803@gmail.com>
2023-06-18 19:27:35 +0530

    Changing some sytles Giving some finishing

--- app.js
+++ app.js
@@ -1,18 +1,4 @@
-// function handleKeyDown(event) {
-//     if (event.key === "Enter") {
-//       event.preventDefault(); // Prevent form submission
-//       search(); // Perform the search
-//       return false;
-//     }
-//     return true;
-//   }
-
-//   function search() {
-//     var query = document.querySelector("input[name=q]").value;
-//     var searchUrl = "https://search.brave.com/search?q=" + encodeURIComponent(query);
-//     window.open(searchUrl, "_blank");
-//   }
-
+// To make the searchbar functioning
 function handleKeyDown(event) {
     if (event.key === "Enter") {
         event.preventDefault();
