9e59fdd85e24b1d6ab0cd6d4d7374b40f0b0a7f9
devharsh007 <hs707803@gmail.com>
2023-06-21 11:45:47 +0530

    Addijng Data.js file for websites collection

---
+++ data.js
@@ -0,0 +1,445 @@
+var websites = [
+    {
+        link: "https://github.com/",
+        cat: "coding",
+        name: "Github"
+    },{
+        link: "https://chat.openai.com/",
+        cat: "coding",
+        name: "Chat GPT"
+    },{
+        link: "www.youtube.com",
+        name: "Youtube"
+    },{
+        link: "https://www.codewars.com/",
+        cat: "coding",
+        name: "CodeWars"
+    },{
+        link: "https://frontendmasters.com/",
+        cat: "coding",
+        name: "Frontend Masters"
+    },{
+        link: "https://codepen.io/your-work",
+        cat: "coding",
+        name: "CodePen"
+    },{
+        link: "https://developer.mozilla.org/en-US/",
+        cat: "coding",
+        name: "MDN"
+    },{
+        link: "https://www.freecodecamp.org/",
+        cat: "coding",
+        name: "FreeCodeCamp"
+    },{
+        link: "https://www.hackerrank.com/",
+        cat: "coding",
+        name: "HackerRank"
+    },{
+        link: "www.codewars.com",
+        cat: "coding",
+        name: "CodeWars"
+    },{
+        link: "https://www.codecademy.com/",
+        cat: "coding",
+        name: "CodeAcademy"
+    },{
+        link: "https://devdocs.io/",
+        cat: "coding",
+        name: "Devdocs"
+    },{
+        link: "https://www.vanillajavascriptprojects.com/",
+        cat: "coding",
+        name: "JS Projects"
+    },{
+        link: "https://html.com/",
+        cat: "coding",
+        name: "HTML.com"
+    },{
+        link: "https://edabit.com/challenges",
+        cat: "coding",
+        name: "Edabit"
+    },{
+        link: "https://javascript30.com/",
+        cat: "coding",
+        name: "JavaScript 30"
+    },{
+        link: "https://www.codecademy.com/",
+        cat: "coding",
+        name: "CodeAcademy"
+    },{
+        link: "https://www.tutorialspoint.com/",
+        cat: "coding",
+        cat: "",
+        name: "Tutorialspoint"
+    },{
+        link: "https://frontendmasters.com/",
+        cat: "coding",
+        name: "Frontendmasters"
+    },{
+        link: "https://codepen.io/your-work/",
+        cat: "coding",
+        name: "Codepen"
+    },{
+        link: "https://wemakedevs.org/",
+        cat: "coding",
+        name: "WeMakeDevs"
+    },{
+        link: "https://www.hackerrank.com/dashboard",
+        cat: "coding",
+        name: "Hackerrank"
+    },{
+        link: "https://johnsmilga.com/",
+        cat: "coding",
+        name: "Johnsmilga"
+    },{
+        link: "https://www.codewars.com/",
+        cat: "coding",
+        name: "codewars"
+    },{
+        link: "https://roadmap.sh",
+        cat: "coding",
+        name: "Raodmap.sh"
+    },{
+        link: "https: //www.theodinproject.com/paths/full-stack-javascript?",
+        cat: "coding",
+        name: "theodinproject"
+    },{
+        link: "https://fmoviesto.tv/",
+        cat: "movies",
+        name: "Fmovies"
+    },{
+        link: "https://www9.123moviesto.to/",
+        cat: "movies",
+        name: "123moviesto"
+    },{
+        link: "https://cataz.net/",
+        cat: "movies",
+        name: "cataz"
+    },{
+        link: "https://www2.solarmovie.cr/movie/https://pdfstop.com/category/harry-potter-1/",
+        cat: "movies",
+        name: "solarmovie"
+    },{
+        link: "https://www.putlockers.do/movie/demolition-8084.html",
+        cat: "movies",
+        name: "putlockers"
+    },{
+        link: "https://www.limetorrents.lol/browse-torrents/Movies/",
+        cat: "movies",
+        name: "limetorrents"
+    },{
+        link: "https://rarbgaccessed.org/torrents.php?r=99481836",
+        name: "rarbgaccessed"
+    },{
+        link: "https://eztv.re/showlist/",
+        cat: "movies",
+        
+        name: ""
+    },{
+        link: "https://yts.monster/",
+        cat: "movies",
+        name: "yts"
+    },{
+        link: "https://www.torlock.com/",
+        cat: "movies",
+        name: "torlock"
+    },{
+        link: "https://gtdb.cc/",
+        cat: "movies",
+        name: "gtdb"
+    },{
+        link: "https://katcr.to/",
+        cat: "movies",
+        name: "katcr"
+    },{
+        link: "https://torrentz2.nz/",
+        cat: "movies",
+        name: "torrentz2"
+    },{
+        link: "https://thepiratebay.org/index.html",
+        name: "thepiratebay"
+        cat: "movies",
+    },{
+        link: "https://1337x.to",
+        cat: "movies",
+        name: "1337x"
+    },{
+        link: "https://fitgirl-repacks.site/",
+        name: "fitgirl-repacks"
+    },{
+        link: "https://repack-mechanics.com/",
+        name: "rg-mechanics"
+    },{
+        link: "https://www.shanaproject.com/series/10384/",
+        name: "shanaproject"
+    },{
+        link: "https://www.kingauthor.net/",
+        name: "kingauthor"
+    },{
+        link: "https://archive.org/details/texts",
+        name: "archive.org"
+    },{
+        link: "https://instapdf.in/",
+        
+        name: "instapdf"
+    },{
+        link: "https://www.pdfnotes.co/",
+        name: "pdfnotes"
+    },{
+        link: "https://givegita.com/bhagavad-gita ",
+        name: "givegita"
+    },{
+        link: "https://it-ebooks.info",
+        cat: "",
+        name: "it-ebooks"
+    },{
+        link: "https://www.pdfdrive.com/",
+        name: "pdfdrive"
+    },{
+        link: "https://manybooks.net/",
+        name: "manybooks"
+    },{
+        link: "https://www.globalgreyebooks.com/index.html",
+        name: "globalgreyebooks"
+    },{
+        link: "https://epuborigin.com/",
+        name: "epuborigin"
+    },{
+        link: "https://crackingpatching.com",
+        name: "crackingpatching"
+    },{
+        link: "https://www.photopea.com/",
+        name: "photopea"
+    },{
+        link: "https://ask4pc.net/",
+        name: "ask4pc"
+    },{
+        link: "https://piratepc.me/",
+        name: "piratepc"
+    },{
+        link: "https://customstart.page/",
+        name: "customstart"
+    },{
+        link: "https://fontawesome.com/",
+        name: "fontawesome"
+    },{
+        link: "https://coolors.co/palettes/popular/black",
+        cat: "",
+olors"
+    },{
+        link: "https://icons.getbootstrap.com/",
+        name: "bootstrap-icons"
+    },{
+        link: "",
+        cat: "",
+        name: ""
+    },{
+        link: "",
+        cat: "",
+        name: ""
+    },{
+        link: "",
+        cat: "",
+        name: ""
+    },{
+        link: "",
+        cat: "",
+        name: ""
+    },{
+        link: "",
+        cat: "",
+        name: ""
+    },{
+        link: "",
+        cat: "",
+        name: ""
+    },{
+        link: "",
+        cat: "",
+        name: ""
+    },{
+        link: "",
+        cat: "",
+        name: ""
+    },{
+        link: "",
+        cat: "",
+        name: ""
+    },{
+        link: "",
+        cat: "",
+        name: ""
+    },{
+        link: "",
+        cat: "",
+        name: ""
+    },{
+        link: "",
+        cat: "",
+        name: ""
+    },{
+        link: "",
+        cat: "",
+        name: ""
+    },{
+        link: "",
+        cat: "",
+        name: ""
+    },{
+        link: "",
+        cat: "",
+        name: ""
+    },{
+        link: "",
+        cat: "",
+        name: ""
+    },{
+        link: "",
+        cat: "",
+        name: ""
+    },{
+        link: "",
+        cat: "",
+        name: ""
+    },{
+        link: "",
+        cat: "",
+        name: ""
+    },{
+        link: "",
+        cat: "",
+        name: ""
+    },{
+        link: "",
+        cat: "",
+        name: ""
+    },{
+        link: "",
+        cat: "",
+        name: ""
+    },{
+        link: "",
+        cat: "",
+        name: ""
+    },{
+        link: "",
+        cat: "",
+        name: ""
+    },{
+        link: "",
+        cat: "",
+        name: ""
+    },{
+        link: "",
+        cat: "",
+        name: ""
+    },{
+        link: "",
+        cat: "",
+        name: ""
+    },{
+        link: "",
+        cat: "",
+        name: ""
+    },{
+        link: "",
+        cat: "",
+        name: ""
+    },{
+        link: "",
+        cat: "",
+        name: ""
+    },{
+        link: "",
+        cat: "",
+        name: ""
+    },{
+        link: "",
+        cat: "",
+        name: ""
+    },{
+        link: "",
+        cat: "",
+        name: ""
+    },{
+        link: "",
+        cat: "",
+        name: ""
+    },{
+        link: "",
+        cat: "",
+        name: ""
+    },{
+        link: "",
+        cat: "",
+        name: ""
+    },{
+        link: "",
+        cat: "",
+        name: ""
+    },{
+        link: "",
+        cat: "",
+        name: ""
+    },{
+        link: "",
+        cat: "",
+        name: ""
+    },{
+        link: "",
+        cat: "",
+        name: ""
+    },{
+        link: "",
+        cat: "",
+        name: ""
+    },{
+        link: "",
+        cat: "",
+        name: ""
+    },{
+        link: "",
+        cat: "",
+        name: ""
+    },{
+        link: "",
+        cat: "",
+        name: ""
+    },{
+        link: "",
+        cat: "",
+        name: ""
+    },{
+        link: "",
+        cat: "",
+        name: ""
+    },{
+        link: "",
+        cat: "",
+        name: ""
+    },{
+        link: "",
+        cat: "",
+        name: ""
+    },{
+        link: "",
+        cat: "",
+        name: ""
+    },{
+        link: "",
+        cat: "",
+        name: ""
+    },{
+        link: "",
+        cat: "",
+        name: ""
+    },{
+        link: "",
+        cat: "",
+        name: ""
+    },{
+        link: "",
+        cat: "",
+        name: ""
+    },
+]
