8c44e4f121d466551a444b4404d6b0a3e9f2ec4f
HARSH SHARMA <69196581+devharsh007@users.noreply.github.com>
2023-10-27 12:42:20 +0530

    features added
    Added some features

--- test.js
+++ test.js
@@ -1,505 +1,8 @@
-var websites = [
-    // Coding-related websites
-    {
-        link: "https://github.com/",
-        cat: "coding",
-        name: "Github"
-    },
-    {
-        link: "https://www.codewars.com/",
-        cat: "coding",
-        name: "CodeWars"
-    },
-    {
-        link: "https://frontendmasters.com/",
-        cat: "coding",
-        name: "Frontend Masters"
-    },
-    {
-        link: "https://codepen.io/",
-        cat: "coding",
-        name: "CodePen"
-    },
-    {
-        link: "https://replit.com/",
-        cat: "coding",
-        name: "Replit"
-    },
-    {
-        link: "https://developer.mozilla.org/en-US/",
-        cat: "coding",
-        name: "MDN"
-    },
-    {
-        link: "https://www.freecodecamp.org/",
-        cat: "coding",
-        name: "FreeCodeCamp"
-    },
-    {
-        link: "https://www.hackerrank.com/",
-        cat: "coding",
-        name: "HackerRank"
-    },
-    {
-        link: "https://www.codecademy.com/",
-        cat: "coding",
-        name: "CodeAcademy"
-    },
-    {
-        link: "https://devdocs.io/",
-        cat: "coding",
-        name: "Devdocs"
-    },
-    {
-        link: "https://www.vanillajavascriptprojects.com/",
-        cat: "coding",
-        name: "Vanila JS Projects"
-    },
-    {
-        link: "https://html.com/",
-        cat: "coding",
-        name: "HTML.com"
-    },
-    {
-        link: "https://edabit.com/",
-        cat: "coding",
-        name: "Edabit"
-    },
-    {
-        link: "https://javascript30.com/",
-        cat: "coding",
-        name: "JavaScript 30"
-    },
-    {
-        link: "https://tutgator.com/tutorials/it-software/",
-        cat: "coding",
-        name: "Tutgator"
-    },
-    {
-        link: "https://johnsmilga.com/",
-        cat: "coding",
-        name: "John Smilga"
-    },
-    {
-        link: "https://www.codewars.com/",
-        cat: "coding",
-        name: "CodeWars"
-    },
-    {
-        link: "https://www.codecademy.com/",
-        cat: "coding",
-        name: "CodeAcademy"
-    },
-    {
-        link: "https://codedamn.com/dashboard",
-        cat: "coding",
-        name: "codedamn"
-    },
-    {
-        link: "https://www.javascripttutorial.net/",
-        cat: "coding",
-        name: "JavaScript Tutorial"
-    },
-    {
-        link: "https://www.30dayscoding.com/",
-        cat: "coding",
-        name: "30 Days Coding"
-    },
-    {
-        link: "https://curiousdevelopers.in/",
-        cat: "coding",
-        name: "Curious Developers"
-    },
-    {
-        link: "https://zerotomastery.io/",
-        cat: "coding",
-        name: "Zero to Mastery"
-    },
-    {
-        link: "https://www.codementor.io/",
-        cat: "coding",
-        name: "CodeMentor"
-    },
-    {
-        link: "https://dailylogochallenge.com/",
-        cat: "coding",
-        name: "Daily Logo Challenge"
-    },
-    {
-        link: "https://threejs.org/",
-        cat: "coding",
-        name: "Three.js"
-    },
-    {
-        link: "https://javascript30.com/",
-        cat: "coding",
-        name: "JavaScript 30"
-    },
-    {
-        link: "https://hacksnation.com/",
-        cat: "coding",
-        name: "hacksnation"
-    },
-    {
-        link: "https://vitejs.dev/",
-        cat: "coding",
-        name: "vite"
-    },
-    {
-        link: "https://scrimba.com/bootcamp",
-        cat: "coding",
-        name: "scrimba"
-    },
-    {
-        link: "https://app.netlify.com/teams/devharsh007/overview",
-        cat: "coding",
-        name: "app.netlify"
-    },
-    {
-        link: "https://codesandbox.io",
-        cat: "coding",
-        name: "codesandbox"
-    },
-    {
-        link: "https://stackblitz.com",
-        cat: "coding",
-        name: "stackblitz"
-    },
-    {
-        link: "https://eloquentjavascript.net/",
-        cat: "coding",
-        name: "eloquentjavascript"
-    },
-    {
-        link: "https://dev.to/",
-        cat: "coding",
-        name: "Dev.to"
-    },
-    {
-        link: "https://www.100jsprojects.com/",
-        cat: "coding",
-        name: "100jsprojects"
-    },
-    {
-        link: "https://claude.ai",
-        cat: "AI",
-        name: "Cloude AI"
-    },
-    {
-        link: "https://pomofocus.io/",
-        cat: "browse",
-        name: "PomoFocus"
-    },
-    {
-        link: "https://www.javascripttutorial.net/",
-        cat: "coding",
-        name: "javascripttutorial"
-    },
-    {
-        link: "https://threejs.org/",
-        cat: "coding",
-        name: "threejs"
-    },
-    {
-        link: "https://in.indeed.com/",
-        cat: "browse",
-        name: "indeed"
-    },
-    
+import { websites } from "./app-data.js";
 
-    // AI-related websites
-    {
-        link: "https://www.bing.com/search?q=cricket+live+scores&form=CPWA",
-        cat: "AI",
-        name: "Bing"
-    },
-    {
-        link: "https://chat.openai.com/",
-        cat: "AI",
-        name: "Chat GPT"
-    },
-    {
-        link: "https://www.useblackbox.io/chat",
-        cat: "AI",
-        name: "BlackBox AI"
-    },
-
-    // Browse-related websites
-    {
-        link: "https://www.youtube.com",
-        cat: "browse",
-        name: "Youtube"
-    },
-    
-
-    // Movies-related websites
-    {
-        link: "https://fmoviesto.tv/",
-        cat: "movies",
-        name: "Fmovies"
-    },
-    {
-        link: "https://www9.123moviesto.to/",
-        cat: "movies",
-        name: "123Movies"
-    },
-    {
-        link: "https://cataz.net/",
-        cat: "movies",
-        name: "Cataz"
-    },
-    {
-        link: "https://www2.solarmovie.cr",
-        cat: "movies",
-        name: "SolarMovie"
-    },
-    {
-        link: "https://www.putlockers.do",
-        cat: "movies",
-        name: "Putlockers"
-    },
-    {
-        link: "https://animesuge.to/home",
-        cat: "movies",
-        name: "animesuge.to"
-    },
-    {
-        link: "https://aniwatch.to/",
-        cat: "movies",
-        name: "aniwatch.to"
-    },
-    {
-        link: "https://mangaplus.shueisha.co.jp/updates",
-        cat: "movies",
-        name: "mangaplus.shueisha"
-    },
-
-    // Torrent-related websites
-    {
-        link: "https://www.limetorrents.lol/",
-        cat: "torrent",
-        name: "LimeTorrents"
-    },
-    {
-        link: "https://rarbgaccessed.org/",
-        cat: "torrent",
-        name: "RARBG"
-    },
-    {
-        link: "https://eztv.re/",
-        cat: "torrent",
-        name: "EZTV"
-    },
-    {
-        link: "https://yts.monster/",
-        cat: "torrent",
-        name: "YTS"
-    },
-    {
-        link: "https://www.torlock.com/",
-        cat: "torrent",
-        name: "TorLock"
-    },
-    {
-        link: "https://gtdb.cc/",
-        cat: "torrent",
-        name: "GTDB"
-    },
-    {
-        link: "https://katcr.to/",
-        cat: "torrent",
-        name: "KATCR"
-    },
-    {
-        link: "https://torrentz2.nz/",
-        cat: "torrent",
-        name: "Torrentz2"
-    },
-    {
-        link: "https://thepiratebay.org/index.html",
-        cat: "torrent",
-        name: "The Pirate Bay"
-    },
-    {
-        link: "https://1337x.to",
-        cat: "torrent",
-        name: "1337x"
-    },
-
-    // Games-related websites
-    {
-        link: "https://fitgirl-repacks.site/",
-        cat: "games",
-        name: "FitGirl Repacks"
-    },
-    {
-        link: "https://repack-mechanics.com/",
-        cat: "games",
-        name: "Repack Mechanics"
-    },
-    {
-        link: "https://www.shanaproject.com/",
-        cat: "games",
-        name: "ShanaProject"
-    },
-
-    // Study-related websites
-    {
-        link: "https://www.kingauthor.net/",
-        cat: "study",
-        name: "KingAuthor"
-    },
-    {
-        link: "https://archive.org/",
-        cat: "study",
-        name: "Archive.org"
-    },
-    {
-        link: "https://instapdf.in/",
-        cat: "study",
-        name: "InstaPDF"
-    },
-    {
-        link: "https://www.pdfnotes.co/",
-        cat: "study",
-        name: "PDFNotes"
-    },
-    {
-        link: "https://givegita.com/",
-        cat: "study",
-        name: "GiveGita"
-    },
-    {
-        link: "https://it-ebooks.info",
-        cat: "study",
-        name: "IT-Ebooks"
-    },
-    {
-        link: "https://www.pdfdrive.com/",
-        cat: "study",
-        name: "PDFDrive"
-    },
-    {
-        link: "https://manybooks.net/",
-        cat: "study",
-        name: "ManyBooks"
-    },
-    {
-        link: "https://www.globalgreyebooks.com/index.html",
-        cat: "study",
-        name: "Global Grey Ebooks"
-    },
-    {
-        link: "https://epuborigin.com/",
-        cat: "study",
-        name: "Epub Origin"
-    },
-    {
-        link: "https://pll.harvard.edu/",
-        cat: "study",
-        name: "harvard.edu"
-    },
-
-    // Softwares-related websites
-    {
-        link: "https://crackingpatching.com",
-        cat: "softwares",
-        name: "CrackingPatching"
-    },
-    {
-        link: "https://www.photopea.com/",
-        cat: "softwares",
-        name: "Photopea"
-    },
-    {
-        link: "https://ask4pc.net/",
-        cat: "softwares",
-        name: "Ask4pc"
-    },
-    {
-        link: "https://piratepc.me/",
-        cat: "softwares",
-        name: "PiratePC"
-    },
-    {
-        link: "https://up4pc.com/",
-        cat: "softwares",
-        name: "Up4PC"
-    },
-    {
-        link: "https://getintopc.com/",
-        cat: "softwares",
-        name: "GetIntoPC"
-    },
-
-    // Design-related websites
-    {
-        link: "https://customstart.page/",
-        cat: "design",
-        name: "CustomStart"
-    },
-    {
-        link: "https://fontawesome.com/",
-        cat: "design",
-        name: "FontAwesome"
-    },
-    {
-        link: "https://coolors.co/",
-        cat: "design",
-        name: "Coolors"
-    },
-    {
-        link: "https://icons.getbootstrap.com/",
-        cat: "design",
-        name: "Bootstrap Icons"
-    },
-    {
-        link: "https://pixabay.com/",
-        cat: "design",
-        name: "Pixabay"
-    },
-    {
-        link: "https://www.svgrepo.com/",
-        cat: "design",
-        name: "SVGRepo"
-    },
-    {
-        link: "https://www.flaticon.com/",
-        cat: "design",
-        name: "Flaticon"
-    },
-    {
-        link: "https://css-tricks.com/",
-        cat: "design",
-        name: "CSS-Tricks"
-    },
-    {
-        link: "https://iconify.design/",
-        cat: "design",
-        name: "Iconify.design"
-    },
-
-    // Tool-related websites
-    {
-        link: "https://tinywow.com/",
-        cat: "tool",
-        name: "TinyWow"
-    },
-    {
-        link: "https://www.ilovepdf.com/",
-        cat: "tool",
-        name: "ILovePDF"
-    },
-    {
-        link: "https://onlinetools.com/",
-        cat: "tool",
-        name: "OnlineTools"
-    }
-];
-let headContainer, categaryList, webContainer;
-
+let headContainer, categaryList, webContainer, dateElement,greetElement;
 function websitesLoader(cat) {
-        // console.log(cat);
+        console.log(cat);
         let webArray = websites.filter((x) => x.cat === cat);
         console.log(webArray);
         webContainer.innerHTML = webArray.map((website) => {
@@ -508,24 +11,25 @@ function websitesLoader(cat) {
             `;
         });
         console.log(webContainer);
-        
-    }
+}
+// DOMContentLoaded Functions
 document.addEventListener("DOMContentLoaded", function () {
     
     headContainer = document.querySelector('#head_container');
     categaryList = document.querySelector('#categary_list');
     webContainer = document.querySelector('#web_container');
+    dateElement = document.querySelector('#date_Element');
+    greetElement = document.querySelector('#greet_Element');
     
-    // dateRenderer();
-    // greetings();
-    
+    dateRenderer(dateElement);
+    greetings(greetElement);
+
     function DataRenderer() {
         categaryList.innerHTML = categaryFilter().map((x) =>
             `<button id=${x} onclick = "websitesLoader('${x}')">${x}</button>`
         ).join('');
     };
-    DataRenderer();
-    
+    DataRenderer(); 
     
     // Filter Categary
     function categaryFilter() {
@@ -534,28 +38,29 @@ document.addEventListener("DOMContentLoaded", function () {
     
 });
 
-// function dateRenderer() {
-//     let date = new Date();
-//     let dateOptions = {
-//         weekday: "long",
-//         year: "numeric",
-//         month: "long",
-//         day: "numeric"
-//     };
-//     return date.toLocaleDateString("en-GB", dateOptions);
+function dateRenderer(x) {
+    let date = new Date();
+    let dateOptions = {
+        weekday: "long",
+        year: "numeric",
+        month: "long",
+        day: "numeric"
+    };
+    return x.innerHTML = date.toLocaleDateString("en-GB", dateOptions);
+}
 
-// }
-// // console.log(dateRenderer());
+function greetings(x) {
+    const timeOfDay = (new Date()).getHours();
+    if (timeOfDay < 6) {
+        return x.innerHTML = 'Good night';
+    } else if (timeOfDay < 12) {
+        return x.innerHTML = 'Good morning';
+    } else if (timeOfDay < 18) {
+        return x.innerHTML = 'Good afternoon';
+    } else {
+        return x.innerHTML = 'Good evening';
+    }
+}
 
-// function greetings() {
-//     const timeOfDay = (new Date()).getHours();
-//     if (timeOfDay < 6) {
-//         return 'Good night';
-//     } else if (timeOfDay < 12) {
-//         return 'Good morning';
-//     } else if (timeOfDay < 18) {
-//         return 'Good afternoon';
-//     } else {
-//         return 'Good evening';
-//     }
-// }
+function startRender() {
+}
