6859c4bfaf7a42f319bae3cd59285b6111a4e66c
HARSH SHARMA <69196581+devharsh007@users.noreply.github.com>
2023-11-11 16:23:42 +0530

    Setting the JSON file format to a new way.

--- test.js
+++ test.js
@@ -1,22 +1,16 @@
-import { websites } from "./app-data.js";
+import { websitesData } from "./app-data.js";
 
-let headContainer, categaryList, webContainer, dateElement,greetElement,timeElement;
-function websitesLoader(cat) {
-        console.log(cat);
-        let webArray = websites.filter((x) => x.cat === cat);
-        console.log(webArray);
-        webContainer.innerHTML = webArray.map((website) => {
-            return `
-            <a href = "${website.link}">${website.name}</a>
-            `;
-        }).join('');
-        console.log(webContainer);
-}
+let headContainer,
+    categaryList,
+    webContainer,
+    dateElement,
+    greetElement,
+    timeElement;
 
 
 // DOMContentLoaded Functions
 document.addEventListener("DOMContentLoaded", function () {
-    
+
     headContainer = document.querySelector('#head_container');
     categaryList = document.querySelector('#categary_list');
     webContainer = document.querySelector('#web_container');
@@ -24,46 +18,79 @@ document.addEventListener("DOMContentLoaded", function () {
     timeElement = document.querySelector("#time_Element");
     greetElement = document.querySelector('#greet_Element');
     const searchInput = document.querySelector('.search-cat');
-    
-    dateRenderer(dateElement,timeElement);
-    greetings(greetElement);
 
-    //Categary Render
-    function DataRenderer() {
-        categaryList.innerHTML = categaryFilter().map((x) =>
-            `<button class="cat_btn" id=${x}>${x}</button>`
-        ).join('');
-    };
-    DataRenderer(); 
-    
+    setInterval(() => {
+        dateRenderer(dateElement, timeElement)
+    }, 1000);
+    greetings(greetElement);
+    categaryFilter();
+    // // Categary Render Function
+    // function DataRenderer() {
+    //     categaryList.innerHTML = categaryFilter().map((x) =>
+    //         `<button class="cat_btn" id=${x}>${x}</button>`
+    //     ).join('');
+    // };
+    // DataRenderer();
+
     // Filter Categary
+
     function categaryFilter() {
-        return Array.from(new Set(websites.map((x) => x.cat)))
+        let catArray = Object.keys(websitesData);
+        // console.log(catArray);
+        for (let i of catArray) {
+            categaryList.innerHTML +=
+                `<button id=${i}>${i}</Button > `
+        }
     }
+
+
     // Event listener for buttons
     categaryList.addEventListener('click', (e) => {
         if (e.target && e.target.nodeName == 'BUTTON') {
-            const category = e.target.id;
+            let category = e.target.id;
             websitesLoader(category);
         }
     })
 
     // Event listener for search
     searchInput.addEventListener('input', (e) => {
-        // Converting to lowercase for case insensitive search
+        // Converting to lowercase for case-insensitive search
         const searchValue = e.target.value.toLowerCase();
-        const resultWebsites = websites.filter((website) => website.name.toLowerCase().includes(searchValue));
 
-        webContainer.innerHTML = resultWebsites.map((website) => {
-            return `
-            <a href = "${website.link}">${website.name}</a>
-            `;
-        }).join('');
+        // Clear previous search results
+        webContainer.innerHTML = '';
+
+        // Iterate over categories
+        Object.values(websitesData).forEach((category) => {
+            // Filter websites within each category
+            const resultWebsites = category.filter((website) =>
+                website.name.toLowerCase().includes(searchValue)
+            );
+
+            // Update the web container with the search results for each category
+            const htmlContent = resultWebsites.map((website) => {
+                return `<a href="${website.link}">${website.name}</a>`;
+            }).join('');
+
+            // Display the search results for each category
+            webContainer.innerHTML += htmlContent;
+        });
     });
+
 });
 
-// Date and Greet Functions
-function dateRenderer(x,y) {
+function websitesLoader(cat) {
+    let catArray = websitesData[cat];
+    webContainer.innerHTML = catArray.map((website) => {
+        return `
+            <a href = "${website.link}">${website.name}</a>
+            `;
+    }).join('');
+    console.log(webContainer);
+}
+
+//? Date and Greet Functions
+function dateRenderer(x, y) {
     let date = new Date();
     let dateOptions = {
         weekday: "long",
@@ -76,7 +103,6 @@ function dateRenderer(x,y) {
     y.innerHTML = time.toUpperCase();
     return;
 }
-
 function greetings(x) {
     const timeOfDay = (new Date()).getHours();
     if (timeOfDay < 6) {
